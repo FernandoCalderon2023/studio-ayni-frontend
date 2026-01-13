b32c6d7cb62fe2deef54a8bb6eff1441cfe8762e
Fernando Calderon <fernando.calderon@fogsphere.com>
2026-01-13 12:20:30 -0400

    Remove GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/studio-ayni-frontend/', // ← Cambiar por el nombre de tu repo
+  base: '/studio-ayni-frontend/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+    assetsDir: 'assets'
   }
 })
